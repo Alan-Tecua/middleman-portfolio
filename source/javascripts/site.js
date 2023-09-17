4c56c8a726cd610ab88558304ce20488581c04ca
alan tecuapetla <alan.tecuapetla@gmail.com>
2023-09-18 00:03:53 +0900

    fixed the carousels, text is now jsutified

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,34 +1,69 @@
 // This is where it all goes :)
-let slideIndex = 1;
+
+
+// fitit
+let slideIndex1 = 1;
 
   // Next/previous controls
-function plusSlides(n) {
-    showSlides(slideIndex += n);
+function plusSlides1(n) {
+    showSlides1(slideIndex1 += n);
 }
 
   // Thumbnail image controls
-function currentSlide(n) {
-    showSlides(slideIndex = n);
+function currentSlide1(n) {
+    showSlides1(slideIndex1 = n);
 }
 
-function showSlides(n) {
+function showSlides1(n) {
     let i;
-    let slides = document.getElementsByClassName("mySlides");
-    let dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
+    let slides = document.getElementsByClassName("fitit");
+    let dots = document.getElementsByClassName("dot-fitit");
+    if (n > slides.length) {slideIndex1 = 1}
+    if (n < 1) {slideIndex1 = slides.length}
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
+    slides[slideIndex1-1].style.display = "block";
+    dots[slideIndex1-1].className += " active";
+}
+
+
+
+// canvas
+let slideIndex2 = 1;
+
+  // Next/previous controls
+function plusSlides2(n) {
+    showSlides2(slideIndex2 += n);
+}
+
+  // Thumbnail image controls
+function currentSlide2(n) {
+    showSlides2(slideIndex2 = n);
+}
+
+function showSlides2(n) {
+    let i;
+    let slides = document.getElementsByClassName("canvas");
+    let dots = document.getElementsByClassName("dot-canvas");
+    if (n > slides.length) {slideIndex2 = 1}
+    if (n < 1) {slideIndex2 = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex2-1].style.display = "block";
+    dots[slideIndex2-1].className += " active";
 }
 
 // show the images on loading site
 window.onload = function() {
-    console.log("hello from JS");
-    showSlides(slideIndex);
+  console.log("hello from JS");
+  showSlides1(slideIndex1);
+  showSlides2(slideIndex2);
 }
