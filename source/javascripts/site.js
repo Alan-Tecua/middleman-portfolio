525b851776fe99ea766115a0746851a8c50ac4a7
alan tecuapetla <alan.tecuapetla@gmail.com>
2023-11-14 22:04:48 +0900

    added new carousel

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,6 +1,3 @@
-// This is where it all goes :)
-
-
 // fitit
 let slideIndex1 = 1;
 
@@ -31,15 +28,12 @@ function showSlides1(n) {
 }
 
 
-
 // canvas
 let slideIndex2 = 1;
-
   // Next/previous controls
 function plusSlides2(n) {
     showSlides2(slideIndex2 += n);
 }
-
   // Thumbnail image controls
 function currentSlide2(n) {
     showSlides2(slideIndex2 = n);
@@ -61,9 +55,37 @@ function showSlides2(n) {
     dots[slideIndex2-1].className += " active";
 }
 
+// weather
+let slideIndex3 = 1;
+  // Next/previous controls
+function plusSlides3(n) {
+    showSlides3(slideIndex3 += n);
+}
+  // Thumbnail image controls
+function currentSlide3(n) {
+    showSlides3(slideIndex3 = n);
+}
+
+function showSlides3(n) {
+    let i;
+    let slides = document.getElementsByClassName("weatherAPI");
+    let dots = document.getElementsByClassName("dot-weather");
+    if (n > slides.length) {slideIndex3 = 1}
+    if (n < 1) {slideIndex3 = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex3-1].style.display = "block";
+    dots[slideIndex3-1].className += " active";
+}
+
 // show the images on loading site
 window.onload = function() {
   console.log("hello from JS");
   showSlides1(slideIndex1);
   showSlides2(slideIndex2);
+  showSlides3(slideIndex3);
 }
